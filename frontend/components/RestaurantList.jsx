1d09af1f57f18769661bc9a7da6dbde5eed098a1
Gemma Fong <115029730+smolTypo@users.noreply.github.com>
2023-07-05 15:20:14 -0400

    Update RestaurantList.jsx

--- frontend/components/RestaurantList.jsx
+++ frontend/components/RestaurantList.jsx
@@ -32,7 +32,7 @@ function RestaurantCard({ data }) {
           className="w-full rounded-2xl"
           height={300}
           width={300}
-          src={`${process.env.STRAPI_URL || "https://strapi-7u75.onrender.co"}${
+          src={`${process.env.STRAPI_URL || "https://strapi-7u75.onrender.com"}${
             data.attributes.image.data[0].attributes.url
           }`}
           alt=""
