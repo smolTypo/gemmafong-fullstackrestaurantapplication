24c3a94ce7768f69484bfd24bb26ba845f58b232
Gemma Fong <115029730+smolTypo@users.noreply.github.com>
2023-07-04 22:31:32 -0400

    Update server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('aW2olD4It8bifkvZIU2u+g==,QJhqnaA00qbYAhI20SjxJw==,zWOcDmYfk5qURWd9NOLldw==,3to5BodorPBGvtj4qD+hdA=='),
+    keys: env.array('aW2olD4It8bifkvZIU2u+g==','QJhqnaA00qbYAhI20SjxJw==','zWOcDmYfk5qURWd9NOLldw==','3to5BodorPBGvtj4qD+hdA=='),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
