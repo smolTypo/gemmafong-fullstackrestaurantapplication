7c4d60c338c5b1f02c6ae4ec822326174be1a110
Gemma Fong <115029730+smolTypo@users.noreply.github.com>
2023-07-09 19:29:08 -0400

    Update Layout.jsx

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -76,7 +76,7 @@ function Navigation() {
 function Footer() {
   return (
     <footer className="bg-green-800 text-white py-4 text-center">
-      Capstone Full Stack Restaurant Application Project developed by Gemma Fong as part of MIT xPRO Full Stack Development with MERN. <a href="https://github.com/smolTypo/gemmafong-fullstackrestaurantapplication">Repository on GitHub</a>.
+      Capstone Full Stack Restaurant Application Project developed by <a href="https://www.linkedin.com/in/gemmafong/">Gemma Fong</a> as part of MIT xPRO Full Stack Development with MERN. <a href="https://github.com/smolTypo/gemmafong-fullstackrestaurantapplication">Repository on GitHub</a>.
     </footer>
   );
 }
