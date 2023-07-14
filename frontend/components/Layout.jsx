3684240bcca2ed3267eb3da53186513fc010b8e7
Gemma Fong <serendikitty@gmail.com>
2023-07-13 20:51:59 -0400

    codemod new-link

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -22,19 +22,25 @@ function Navigation() {
       <nav className="flex justify-between p-6 px-4">
         <div className="flex justify-between items-center w-full mx-16">
           <div className="xl:w-1/3">
-            <Link href="/" passHref>
-              <a className="block text-3xl max-w-max text-white font-semibold">
+            <Link
+              href="/"
+              passHref
+              className="block text-3xl max-w-max text-white font-semibold">
+              
                 FoodieEats
-              </a>
+              
             </Link>
           </div>
 
           <div className="xl:block xl:w-1/3">
             <div className="flex items-center justify-end">
-              <Link href="/" passHref>
-                <a className="text-gray-50 hover:text-yellow-200 font-semibold text-lg">
+              <Link
+                href="/"
+                passHref
+                className="text-gray-50 hover:text-yellow-200 font-semibold text-lg">
+                
                   Home
-                </a>
+                
               </Link>
 
               <div className="hxl:block">
@@ -52,15 +58,21 @@ function Navigation() {
                   </div>
                 ) : (
                   <div className="flex items-center justify-end">
-                    <Link href="/login" passHref>
-                      <a className="inline-block py-2 px-4 mr-2 leading-5 text-gray-50 hover:text-yellow-200 font-semibold bg-transparent rounded-md">
+                    <Link
+                      href="/login"
+                      passHref
+                      className="inline-block py-2 px-4 mr-2 leading-5 text-gray-50 hover:text-yellow-200 font-semibold bg-transparent rounded-md">
+                      
                         Log In
-                      </a>
+                      
                     </Link>
-                    <Link href="/register" passHref>
-                      <a className="inline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-600 hover:bg-green-700 font-semibold focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md">
+                    <Link
+                      href="/register"
+                      passHref
+                      className="inline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-600 hover:bg-green-700 font-semibold focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md">
+                      
                         Sign Up
-                      </a>
+                      
                     </Link>
                   </div>
                 )}
