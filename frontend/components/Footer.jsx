d626a727c28490e69d8e1c8aa819a7b1f0abc048
Gemma Fong <115029730+smolTypo@users.noreply.github.com>
2023-07-22 20:01:12 -0400

    Create Footer.jsx

---
+++ frontend/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-green-800 text-white py-4 text-center">
+      Capstone Full Stack Restaurant Application Project developed by
+      <a href="https://www.linkedin.com/in/gemmafong/">Gemma Fong</a> as part of
+      MIT xPRO Full Stack Development with MERN.
+      <a href="https://github.com/smolTypo/gemmafong-fullstackrestaurantapplication">
+        Repository on GitHub
+      </a>
+      .
+    </footer>
+  );
+};
+
+export default Footer;
