57f12fd454b81b61f819326f9d00987aaf6ef2d0
Gemma Fong <115029730+smolTypo@users.noreply.github.com>
2023-07-22 20:12:59 -0400

    Update Footer.jsx

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 
-const Footer = () => {
+function Footer() {
   return (
     <footer className="bg-green-800 text-white py-4 text-center">
-      Capstone Full Stack Restaurant Application Project developed by
-      <a href="https://www.linkedin.com/in/gemmafong/">Gemma Fong</a> as part of
-      MIT xPRO Full Stack Development with MERN.
-      <a href="https://github.com/smolTypo/gemmafong-fullstackrestaurantapplication">
-        Repository on GitHub
-      </a>
-      .
+      Capstone Full Stack Restaurant Application Project developed by{" "}
+      <a href="https://www.linkedin.com/in/gemmafong/">Gemma Fong</a> as part of MIT xPRO Full Stack Development with MERN.{" "}
+      <a href="https://github.com/smolTypo/gemmafong-fullstackrestaurantapplication">Repository on GitHub</a>.
     </footer>
   );
-};
+}
 
 export default Footer;
